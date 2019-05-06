d35fc935a4bd863d6d17ced6d4627265f6fc5272
Rudeboyb4ss <jorge.sandoval.m@usach.cl>
2019-05-06 02:27:15 -0400

    v2
    Estructura de datos aplicada para solucion de matriz dispersa.

--- Proyecto2.c
+++ Proyecto2.c
@@ -10,67 +10,77 @@ La entrada debe ser un archivo de tipo .in con una matriz con un formato especí
 Las variables se trabajarán como double ya que se trabajo con la función POW que retorna
 un double.
 ****************************************************************************************/
+typedef struct MatrizDispersa{
+   int fila;
+   int Columna;
+   double dato;  
+   struct MatrizDispersa * siguiente; 
+}MD;
 
-//Funcion: calcular determinante.
-//DOM: Matriz (arreglo) 
-//REC: Numero formato long double.
-long double determinante(double ** matriz , int orden){
-	//se definen las variables para almacenar los resultados.
-	long double det = 0 , aux = 0;
-	//Esta variable permitirá rellenar la matriz cofactor.
-	int ColumanaAux;
-	//Caso base: Determinante de una matriz de 2x2.
-	if (orden == 2)
-		return (matriz[0][0]*matriz[1][1] - matriz[1][0]*matriz[0][1]);
-	
-	else{
-		//Se inicia una variable de 0 hasta el orden actual.
-		int j;
-		for(j=0; j < orden;j++){
-			//Se crea la matriz cofactor por cada vuelta del ciclo pero de un orden menor.
-			double**cofactor = (double **)malloc(sizeof(double)*(orden-1)); 
-			int i;
-   			for (i=0; i<(orden-1);i++) 
-      			cofactor[i] = (double*)malloc(sizeof(double)*(orden-1)); 
-      	//se recorren las filas.
-      	int fila;
-      	for (fila=1; fila < orden; fila++){
-      		//se reinicia la variable columna aux.
-      		ColumanaAux = 0;
-      		//se recorren las columnas
-      		int columna;
-      		for (columna=0; columna < orden; columna++){
-      			//se establece cual será la fila y columna que se eliminará.
-      			if (columna != j){
-      				//se rellena la matriz cofactor.
-      				cofactor[fila-1][ColumanaAux] = matriz[fila][columna];
-      				ColumanaAux++;
-      			}
-      		}
-      	}
-      	//se realiza la operacion donde se establece el determinante, el signo segun matriz de signos 
-      	//y se hace el llamado recursivo.
-      	aux = (pow(-1, 2+j) * matriz[0][j] * determinante(cofactor,orden-1));
-      	det = det + aux;
+MD * primero, *ultimo;
 
-      	//Se libera memoria reservada para la matriz cofactor.
-      	int q;
-      	for (q=0; q < orden-1 ;q++)
-      		free(cofactor[q]);
-      	free(cofactor);
-		}
-	//Se retorna el determinante obtenido en el caso else.
-	return det;
-	}
+//Con esta función se añade un elemento al final de la lista.
+
+void añadir(int filaActual, int columnaActual, double datoActual) {
+   MD *nuevo;
+   // reservamos memoria para el nuevo elemento.
+   nuevo = (MD *) malloc (sizeof(MD));
+   /*if (nuevo==NULL) 
+      printf( "No hay memoria disponible!\n");*/
+   //seguarda la fila.
+   nuevo->fila = filaActual;
+   //se guarda la columna.
+   nuevo->Columna = columnaActual;
+   //se guarda el dato.
+   nuevo-> dato = datoActual;
+   //el campo siguiente va a ser NULL por ser el último elemento de la lista
+   nuevo->siguiente = NULL;
+ 
+   // ahora metemos el nuevo elemento en la lista. lo situamos al final de la lista.
+   //comprobamos si la lista está vacía.
+      if (primero==NULL) {
+         primero = nuevo;
+         ultimo = nuevo;
+      }
+      else {
+      // el que hasta ahora era el último tiene que apuntar al nuevo.
+         ultimo->siguiente = nuevo;
+      // hacemos que el nuevo sea ahora el último.
+         ultimo = nuevo;
+      }
+ }
+ 
+ void mostrar() {
+      MD *auxiliar; // lo usamos para recorrer la lista.
+      int i;
+ 
+      i=0;
+      auxiliar = primero;
+      printf("\nMostrando la lista completa:\n");
+
+      while (auxiliar!=NULL) {
+            printf( "[fila %d,columna %d, dato %f\n",auxiliar->fila,auxiliar->Columna,auxiliar->dato);
+            auxiliar = auxiliar->siguiente;
+            i++;
+      }
+      if (i==0) printf( "\nLa lista está vacía!!\n" );
+ }
+
+
+void powMatriz(){
+
 }
 
 //Funcion: Leer Matriz 
 //DOM: archivo matriz.in
 //REC: void
 void LeerMatriz(){
+
    FILE *fichero;
    //se crea variable para guardar el orden de la matriz.
    int n = 0;
+   int exp = 0;
+
    //se abre el archivo.
    //************************************************
    fichero = fopen("matriz-2333148719.in","r");
@@ -83,41 +93,36 @@ void LeerMatriz(){
       exit (-1);
    }
 
-   //toma el primer valor del archivo para asignar memoria a matriz.
+   //toma el primer valor para guardar el tamaño de la matriz.
    fscanf (fichero, "%d", &n);
+   //toma el segundo valor para saber a que numero se elevará la matriz.
+   fscanf (fichero, "%d", &exp);
    //se reserva memoria para la matriz.
-   double ** matriz = (double **)malloc(sizeof(double)*n); 
-   int i;
-   for (i=0; i<n;i++) 
-      matriz[i] = (double*)malloc(sizeof(double)*n); 
 
    //leer datos y rellenar matriz 
+   int i;
    for (i=0; i < n;++i){
    		int j;
       for (j=0; j < n;++j){
-         fscanf(fichero,"%lf", &matriz[i][j]);
+         double DatoLeido;
+         fscanf(fichero,"%lf", &DatoLeido);
+         if(DatoLeido != 0){
+            añadir(i,j,DatoLeido);
+         }
+
       }
    }
    //Se cierra el archivo.
    fclose(fichero);
-   //se imprime la matriz para que el cliente pueda verificar errores.
-   printf( "\n\nMatriz proporcionada:\n" );
-   for (i = 0; i < n; i++) {
-   		int j;
-      for (j = 0; j < n; j++)
-	 printf ("%d ", (int)matriz[i][j]);
-      printf ("\n");
-   }
-   printf("Esta matriz es de (%dx%d).\n",n,n);
 
-   //Se calcula el determinante, entregando la matriz y el orden.
-   long double det = determinante(matriz, n);
-   //Se muentra el resultado obtenido.
-   printf("\nEl determinante obtenido ha sido: %ld. \n\n",(long int)det);
+
 }
 
 int main(int argc, char const *argv[]){
+   primero = (MD *) NULL;
+   ultimo = (MD *) NULL;
 	//Se llama a la funcion que lee y realiza llamados de procesamiento.
 	LeerMatriz();
+   mostrar();
 	return 0;
 }
